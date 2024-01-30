5fdf7ab17c5f2891ff187e7ad0b4cc5959cbbee3
Jenny Christie <jchristie@Jennys-MacBook-Pro.local>
2024-01-30 11:19:02 -0500

    adding new grid and text into Footer component

--- src/layout/MainLayout/Footer.tsx
+++ src/layout/MainLayout/Footer.tsx
@@ -1,10 +1,14 @@
 // material-ui
-import { Link, Stack, Typography } from '@mui/material';
+import { Link, /*Stack,*/ Grid, Typography } from '@mui/material';
 
 const Footer = () => (
-  <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ p: '24px 16px 0px', mt: 'auto' }}>
-    <Typography variant="caption">&copy; All rights reserved</Typography>
-    <Stack spacing={1.5} direction="row" justifyContent="space-between" alignItems="center">
+  /*<Grid direction="column" justifyContent="space-between" alignItems="left" sx={{ p: '24px 16px 0px', mt: 'auto'}}>*/
+  <Grid container sx={{ p: '24px 16px 0px', mt: 'auto'}}>
+    <Grid item xs={3}>
+      <b>The TORCHLITE Dashboard and the <Link href="https://htrc.atlassian.net/wiki/spaces/COM/pages/43295914/Extracted+Features+v.2.0" target="_blank">Extracted Features Dataset</Link> that powers the Dashboard are products of the <Link href="https://analytics.hathitrust.org/" target="_blank">HathiTrust Research Center</Link>.</b>
+    </Grid>
+    {/*<Typography variant="caption">The TORCHLITE Dashboard and the Extracted Features Dataset that powers the Dashboard are products of the HathiTrust Research Center.</Typography>*/}
+    {/*<Stack spacing={1.5} direction="row" justifyContent="space-between" alignItems="right">
       <Link href="#" target="_blank" variant="caption" color="textPrimary">
         About us
       </Link>
@@ -14,8 +18,12 @@ const Footer = () => (
       <Link href="#" target="_blank" variant="caption" color="textPrimary">
         Terms
       </Link>
-    </Stack>
-  </Stack>
+</Stack>*/}
+  </Grid>
+
+  //Using Grid for new footer instead of Stack, since we need more chunks of text at different widths.
+ 
+
 );
 
 export default Footer;
