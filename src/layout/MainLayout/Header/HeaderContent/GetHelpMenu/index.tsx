4d9cd1aacd39720686eef8789d0b036431d7b100
Jenny Christie <jchristie@Jennys-MacBook-Pro.local>
2024-02-08 12:18:32 -0500

    adjusting Get Help and Footer components for dark mode

--- src/layout/MainLayout/Header/HeaderContent/GetHelpMenu/index.tsx
+++ src/layout/MainLayout/Header/HeaderContent/GetHelpMenu/index.tsx
@@ -30,7 +30,7 @@ const StyledMenu = styled((props: MenuProps) => (
     marginTop: theme.spacing(1),
     minWidth: 150,
     color:
-      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.common.white,
     boxShadow:
       'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
     '& .MuiMenu-list': {
